7ccce704ea88b120d00f7dc9f752d7e23b40d817
Touni Atchadé <68529247+oratchade@users.noreply.github.com>
2025-08-06 15:44:27 -0400

    Refractor common package to defintion

---
+++ query/definition/constants.go
@@ -0,0 +1,25 @@
+package definition
+
+type QueryType = string
+
+const (
+	QueryTypeSelect   QueryType = "SELECT"
+	QueryTypeInsert   QueryType = "INSERT"
+	QueryTypeUpdate   QueryType = "UPDATE"
+	QueryTypeDelete   QueryType = "DELETE"
+	QueryTypeUpsert   QueryType = "UPSERT"
+	QueryTypeTruncate QueryType = "TRUNCATE"
+)
+
+type DBType = string
+
+const (
+	// DriverMySQL is the driver name for MySQL databases.
+	DriverMySQL DBType = "mysql"
+	// DriverPostgres is the driver name for PostgreSQL databases.
+	DriverPostgres DBType = "postgres"
+	// DriverSQLLite is the driver name for SQLLite databases.
+	DriverSQLLite DBType = "SQLLite3"
+	// DriverMSSQL is the driver name for Microsoft SQL Server databases.
+	DriverMSSQL DBType = "sqlserver"
+)
